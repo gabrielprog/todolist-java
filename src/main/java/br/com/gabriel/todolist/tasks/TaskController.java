1c980d0e866f12915eff8eb6b5ec7737071a2f5e
Gabriel Santos <69699850+gabrielprog@users.noreply.github.com>
2023-10-17 19:22:26 +0000

    Add validations priority and task

--- src/main/java/br/com/gabriel/todolist/tasks/TaskController.java
+++ src/main/java/br/com/gabriel/todolist/tasks/TaskController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,13 @@ public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
+    public boolean isPriorityValid(TaskModel taskModel) {
+        if (taskModel.getPriority() < 1 || taskModel.getPriority() > 3) 
+            return false;
+        
+        return true;
+    }
+    
     @GetMapping("/read")
     public List<TaskModel> read(HttpServletRequest request) {
         return taskRepository.findByUserId((UUID) request.getAttribute("userId"));
@@ -32,20 +40,37 @@ public class TaskController {
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         taskModel.setUserId((UUID) request.getAttribute("userId"));
-        
+
+        if(isPriorityValid(taskModel) == false)
+            return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body("Priority must be between 1 and 3");
+
         return ResponseEntity
         .status(HttpStatus.CREATED)
         .body(taskRepository.save(taskModel));
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-        TaskModel taskId = taskRepository.findById(id).orElseThrow();
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        TaskModel taskId = taskRepository.findById(id).orElse(null);
+
+        if(taskId == null)
+        return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body("Task not found");
         
         taskModel.setUserId((UUID) request.getAttribute("userId"));
         Utils.copyNonNullProperties(taskModel, taskId);
         
-        return taskRepository.save(taskModel);
+        if(isPriorityValid(taskModel) == false)
+            return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body("Priority must be between 1 and 3");
+
+        return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(taskRepository.save(taskId));
     }
 
 }
